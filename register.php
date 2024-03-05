233dafe51cd7a83ba2e5afec39741638ed932b99
Jonas Möller <jonas.moeller23@icloud.com>
2024-03-05 20:20:19 +0100

    Complete stupid test

--- register.php
+++ register.php
@@ -90,7 +90,7 @@ $servername = "localhost";
         if(!$error) {    
             $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
 
-            $sql = "INSERT INTO Costumer (name, lastname, street, zipcode, city, bankaccount, blz, institut, password, email) VALUES ('$name', '$lastname', '$street', $zipcode, '$city', $bankaccount, $blz, '$institut', '$passwort_hash', '$email')";
+            $sql = 'INSERT INTO Costumer (name, lastname, street, zipcode, city, bankaccount, blz, institut, password, email) VALUES ("Jonas", "Moller", "Tannehof", 36103, "Flieden", 123, 123, "Fulda", "123", "mailo")';
             $result = $conn->query($sql);
             echo "('$name', '$lastname', '$street', $zipcode, '$city', $bankaccount, $blz, '$institut', '$passwort_hash', '$email')";
             echo "This is res" . $result;
