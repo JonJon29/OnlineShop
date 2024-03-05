dfaf5bac7b468d3c18d88d28efe407f7001c2d80
Jonas Möller <jonas.moeller23@icloud.com>
2024-03-05 20:44:11 +0100

    Deleted "

--- register.php
+++ register.php
@@ -90,7 +90,7 @@ $servername = "localhost";
         if(!$error) {    
             $passwort_hash = password_hash($passwort, PASSWORD_DEFAULT);
 
-            $sql = 'INSERT INTO Costumer (name, lastname, street, zipcode, city, bankaccount, blz, institut, password, email) VALUES ("' . $name . '", "' . $lastname . '", "' .$street . '", ' . $zipcode . ', "' . $city . '", ' . $bankaccount . ', ' . $blz . ', "' . $institut . '", "' . $passwort_hash . '", "' . $email . '")';
+            $sql = "INSERT INTO Costumer (name, lastname, street, zipcode, city, bankaccount, blz, institut, password, email) VALUES ($name,  $lastname , $street , $zipcode , $city , $bankaccount , $blz, $institut , $passwort_hash , $email)";
             $result = $conn->query($sql);
             echo '("' . $name . '", "' . $lastname . '", "' .$street . '", ' . $zipcode . ', "' . $city . '", ' . $bankaccount . ', ' . $blz . ', "' . $institut . '", "' . $passwort_hash . '", "' . $email . '")';
             echo "This is res" . $result;
