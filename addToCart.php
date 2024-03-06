ec327629cb6e785a46e1aa1c80df8a6f95c03f47
Jonas Möller <jonas.moeller23@icloud.com>
2024-03-06 18:23:37 +0100

    Uch binnnn dummm

--- addToCart.php
+++ addToCart.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["costumerID"]) && isset($_GET['prodID'])){
     $prodID = $_POST['prodID'];
     $costumerID = $_SESSION['costumerID'];
     echo "Hier istg gurt";
-    $sql = "INSERT INTO Cart VALUES (?, ?)";
+    $sql = "INSERT INTO Cart (costumerID, prodID) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('ss', $costumerID, $prodID);
     $result = $stmt->execute();
